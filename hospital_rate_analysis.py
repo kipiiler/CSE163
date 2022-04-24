139048f3d4dc91c8fe9eeef26d7612df43163437
hoang nguyen <hoangng@cs.washington.edu>
2022-04-23 23:13:05 -0700

    hm

--- hospital_rate_analysis.py
+++ hospital_rate_analysis.py
@@ -12,4 +12,4 @@ hospitalRate.groupby("Year").sum().reset_index().plot(x="Year", y="Value")
 
 hospitalRate.groupby("Race/ ethnicity").sum().reset_index().plot(kind="bar",x="Race/ ethnicity", y="Value")
 
-hospitalRate.groupby("Jurisdiction").sum().reset_index().nlargest(10,"Value").plot(kind="bar",x="Jurisdiction", y="Value")
+hospitalRate.groupby("Jurisdiction").sum().reset_index().nlargest(10,"Value").plot(kind="bar",x="Jurisdiction", y="Value")  
